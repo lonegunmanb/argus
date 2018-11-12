174d0fb5764bf91a0d035d5bdc48d890ba50f865
lonegunman <lonegunmanb@hotmail.com>
2018-11-12 16:22:10 +0800

    add unit tests

--- src/test/java/org/apache/uss/argus/operand/JsonObjectGetPropertiesTest.kt
+++ src/test/java/org/apache/uss/argus/operand/JsonObjectGetPropertiesTest.kt
@@ -1,36 +1,59 @@
 package org.apache.uss.argus.operand
 
-import com.google.gson.Gson
+import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.serializer.SerializerFeature
 import org.junit.jupiter.api.Test
-import java.lang.reflect.Type
 import java.math.BigDecimal
 import kotlin.test.assertEquals
 
 class JsonObjectGetPropertiesTest {
-    val gson = Gson()
 
-    @Test
-    fun simpleTest(){
-        val latlong1 = BigDecimal("1.1")
-        val latlong2 = BigDecimal("2.2")
-        val address = Address("address1", "city1", arrayOf(latlong1, latlong2))
-        val jsonObject = JsonObject(gson.toJson(address), "address", null, null)
-        val properties = jsonObject.getProperties()!!
-        assertEquals(3, properties.size)
-        assertEquals("address1", properties.first { pair->pair.first == "address" }.second)
-        assertEquals("city1", properties.first { pair->pair.first == "city"}.second)
-        val array = properties.first { pair->pair.first == "latlong" }.second as Array<*>
-        assertEquals(2, array.size)
-        assertEquals(latlong1, array[0])
-        assertEquals(latlong2, array[1])
+    private operator fun List<Pair<String, Any?>>.get(key:String):Any? {
+        return this.first { pair->pair.first == key }.second
     }
 
-//    @Test
-//    fun simpleTest2() {
-//        data class SubClass(val subNumber:Int?, val subText:String?, val subBool:Boolean?)
-//        data class Class1(val number:Int?, val text:String?, val bool:Boolean?, val subProperty:SubClass?)
-//        val obj = Class1(1, "abc", true, SubClass(2, "dev", true))
-//        //gson.
-//        val json = gson.toJson(obj, Class1::class.java)
-//    }
+    @Suppress("UNCHECKED_CAST")
+    @Test
+    fun simpleTest() {
+        val obj = object{
+            val number = 1
+            val text = "abc"
+            val bool = true
+            val subProperty = object{
+                val subNumber=2
+                val subText = "def"
+                val subBool = false
+            }
+            val nullString:String? = null
+        }
+        val json = JSON.toJSONString(obj)
+        val jsonObject = JsonObject(json, "test", null, null)
+        val properties = jsonObject.getProperties()!!
+        assertEquals(4, properties.size)
+        assertEquals(BigDecimal(obj.number), properties["number"])
+        assertEquals(obj.text, properties["text"])
+        assertEquals(obj.bool, properties["bool"])
+        val subProperty = properties["subProperty"] as List<Pair<String, Any?>>
+        assertEquals(BigDecimal(obj.subProperty.subNumber), subProperty["subNumber"])
+        assertEquals(obj.subProperty.subText, subProperty["subText"])
+        assertEquals(obj.subProperty.subBool, subProperty["subBool"])
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    @Test
+    fun arrayTest() {
+        val arrayItem = object {
+            val name = "abc"
+        }
+        val obj = object {
+            val names = arrayOf(arrayItem, arrayItem)
+        }
+        val json = JSON.toJSONString(obj, SerializerFeature.DisableCircularReferenceDetect)
+        val jsonObject = JsonObject(json, "test", null, null)
+        val properties = jsonObject.getProperties()!!
+        val arrayPair = properties["names"] as Array<*>
+        assertEquals(obj.names.size, arrayPair.size)
+        assertEquals(arrayItem.name, (arrayPair[0] as List<Pair<String, Any?>>)["name"])
+        assertEquals(arrayItem.name, (arrayPair[1] as List<Pair<String, Any?>>)["name"])
+    }
 }
